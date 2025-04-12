8c38337b98aabf3ec7edabb13dd151f324ffbbce
ATHStudioo <ihor6285@gmail.com>
2025-04-12 18:50:47 +0200

    Luauuu

---
+++ scripts.lua
@@ -0,0 +1,15 @@
+-- Возвращаем таблицу скриптов
+return {
+    {
+        name = "test",
+        description = "print a test",
+        gameIds = {0}, -- 0 означает для всех игр
+        script = "https://raw.githubusercontent.com/ATHStudioo/RbxAARR_Hub/refs/heads/main/Scripts/Print_test.lua"
+    },
+    {
+        name = "Fly",
+        description = "Flyyyyy",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"
+    }
+}
