b269c30552ebd3aeadfb96768403e11832ea9267
ATHStudioo <ihor6285@gmail.com>
2025-04-12 19:20:10 +0200

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -8,8 +8,194 @@ return {
     },
     {
         name = "Fly",
-        description = "Flyyyyy",
+        description = "Creates a GUI for fly control.",
         gameIds = {0},
         script = "https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"
+    },
+    {
+        name = "Aimbot Exunys",
+        description = "Aimbot with GUI.",
+        gameIds = {0},
+        script = "https://pastebin.com/raw/HGZaJNxe"
+    },
+    {
+        name = "Infinite Yield",
+        description = "Admin commands script with many utilities.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"
+    },
+    {
+        name = "ESP",
+        description = "Player ESP and tracking.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua"
+    },
+    {
+        name = "CMD-X",
+        description = "Another admin commands script.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source"
+    },
+    {
+        name = "Dark Dex",
+        description = "Game explorer tool for developers.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV4.lua"
+    },
+    {
+        name = "Remote Spy",
+        description = "Spy on remote events and functions.",
+        gameIds = {0},
+        script = "https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"
+    },
+    {
+        name = "Hydroxide",
+        description = "Advanced debugging suite.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/Upbolt/Hydroxide/revision/init.lua"
+    },
+    {
+        name = "Owl Hub",
+        description = "Universal cheat hub for many games.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"
+    },
+    {
+        name = "Noclip",
+        description = "Simple noclip script to walk through walls.",
+        gameIds = {0},
+        script = "https://pastebin.com/raw/pKV2Ys4E"
+    },
+    {
+        name = "Click Teleport",
+        description = "Teleport to where you click.",
+        gameIds = {0},
+        script = "https://pastebin.com/raw/7vYCidUf"
+    },
+    {
+        name = "Anti-AFK",
+        description = "Prevents being kicked for AFK.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind"
+    },
+    {
+        name = "Blox Fruits",
+        description = "Script hub for Blox Fruits game.",
+        gameIds = {2753915549, 4442272183, 7449423635},
+        script = "https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"
+    },
+    {
+        name = "Doors",
+        description = "Script for Doors horror game.",
+        gameIds = {6516141723},
+        script = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Script.lua"
+    },
+    {
+        name = "Speed Hub X",
+        description = "Multi-game script hub.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/ahmadsgamer2/Script--Game/main/Script%20Game"
+    },
+    {
+        name = "VHub",
+        description = "Universal hub with many scripts.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/V31nc/2642/Created/VHub"
+    },
+    {
+        name = "Domain X",
+        description = "Script hub with many features.",
+        gameIds = {0},
+        script = "https://raw.githubusercontent.com/shlexware/DomainX/main/source"
+    },
+    {
+        name = "BTools",
+        description = "Gives building tools.",
+        gameIds = {0},
+        script = "https://pastebin.com/raw/NLMwzrNK"
+    },
+    {
+        name = "Pet Simulator X",
+        description = "Script for Pet Simulator X.",
+        gameIds = {6284583030},
+        script = "https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"
+    },
+    {
+        name = "Adopt Me",
+        description = "Script for Adopt Me.",
+        gameIds = {920587237},
+        script = "https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"
+    },
+    {
+        name = "Brookhaven",
+        description = "Script for Brookhaven RP.",
+        gameIds = {4924922222},
+        script = "https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"
+    },
+    {
+        name = "Murder Mystery 2",
+        description = "Script for MM2.",
+        gameIds = {142823291},
+        script = "https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script"
+    },
+    {
+        name = "Arsenal",
+        description = "Script for Arsenal FPS game.",
+        gameIds = {286090429},
+        script = "https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main"
+    },
+    {
+        name = "Phantom Forces",
+        description = "Script for Phantom Forces.",
+        gameIds = {292439477},
+        script = "https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua"
+    },
+    {
+        name = "Jailbreak",
+        description = "Script for Jailbreak.",
+        gameIds = {606849621},
+        script = "https://raw.githubusercontent.com/KuriWasTaken/MonkeyScripts/main/Jailbreak/JailMonkey.lua"
+    },
+    {
+        name = "Natural Disaster Survival",
+        description = "Script for Natural Disaster Survival.",
+        gameIds = {189707},
+        script = "https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/LoadstringypVvhJBq4QNz7"
+    },
+    {
+        name = "Lumber Tycoon 2",
+        description = "Script for Lumber Tycoon 2.",
+        gameIds = {13822889},
+        script = "https://raw.githubusercontent.com/Kozowe/Lumber-Tycoon-2-Scripts/main/Dupe%20Script"
+    },
+    {
+        name = "Wizard Tycoon",
+        description = "Script for Wacky Wizards.",
+        gameIds = {6516141723},
+        script = "https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main"
+    },
+    {
+        name = "Vehicle Simulator",
+        description = "Script for Vehicle Simulator.",
+        gameIds = {171391948},
+        script = "https://raw.githubusercontent.com/GameGuy6969/roblox-scripts/main/VSHub.lua"
+    },
+    {
+        name = "Da Hood",
+        description = "Script for Da Hood.",
+        gameIds = {2788229376},
+        script = "https://raw.githubusercontent.com/NighterEpic/Faded/main/YesEpic"
+    },
+    {
+        name = "Speed Run 4",
+        description = "Script for Speed Run 4.",
+        gameIds = {183364845},
+        script = "https://raw.githubusercontent.com/i4mitty/MittyGamesHub/main/SpeedRun4.lua"
+    },
+    {
+        name = "Kingdom Hearts",
+        description = "Script for Kingdom Hearts.",
+        gameIds = {6728870912},
+        script = "https://raw.githubusercontent.com/bunnynwy/games/main/KH.lua"
     }
 }
