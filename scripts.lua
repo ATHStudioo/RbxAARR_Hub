638f62ea5bcf4753b4ddc7e6ed260002b6c330b8
ATHStudioo <ihor6285@gmail.com>
2025-04-12 20:22:21 +0200

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,9 +1,8 @@
--- Возвращаем таблицу скриптов
 return {
     {
         name = "test",
         description = "print a test",
-        gameIds = {0}, -- 0 означает для всех игр
+        gameIds = {0},
         script = "https://raw.githubusercontent.com/ATHStudioo/RbxAARR_Hub/refs/heads/main/Scripts/Print_test.lua"
     },
     {
